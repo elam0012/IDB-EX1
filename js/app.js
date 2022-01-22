422754cd0856d727f9d809dbdd6cffcfced20416
Yasser Ehaimer <yehaimer@Yassers-MacBook-Pro.local>
2022-01-22 18:21:47 -0500

    add movies DB

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ const APP = {
   init: (ev) => {
     //page has loaded
     APP.setUpDB();
-    document
-      .getElementById('movieForm')
-      .addEventListener('submit', APP.saveMovie);
+    document.getElementById('movieForm').addEventListener('submit', APP.saveMovie);
   },
   setUpDB: () => {
     //open the db
     //listen for onupgradeneeded
     //create store with keyPath title
     //add 4 movie objects
+    initDB();
+    // APP.getMovies() // not ready to work yet
   },
   saveMovie: (ev) => {
     ev.preventDefault();
@@ -28,10 +28,48 @@ const APP = {
   getMovies: () => {
     //retrieve the list of movies from the database
     //call APP.displayMovies when transaction complete
+
+    let tx = DB.transaction("movieStore")
+
+    tx.oncomplete =  (ev) => {
+      // the transaction is complete... need to do somthing else
+    }
+
+    let movieStore = tx.objectStore('movieStore');
+    let getRequest = movieStore.getAll(); // Request all teh movies
+    // get() getAll() add() delete() set() count()
+    getRequest.onsuccess = (ev) => {
+      // we now have all the movie data
+      let movies = ev.target.result
+      APP.displayMovies(movies)
+    }
+
+    getRequest.onerror = (err) = {
+      //error reding the data
+    }
+
   },
-  displayMovies: () => {
+  displayMovies: (movies) => {
     //called when DB is opened successfully
     //also called from APP.getMovies
+
+    console.log(movies)
+    // loop through movies
+    // let list = document.querySelector("#datalist")
+    // let html = movies.map(movie =>{
+    //   return `<li></li>`
+    // }).join('')
+    // list.append(html)
+
+    // 
+    
+
+    // let list = document.querySelector('#datalist');
+    //     let html = movies.map((movie) => {
+    //         let str = `<p>${movie.title}</p>`;
+    //         return str;
+    //       }).join('');
+    //     list.setHTML(html);
   },
 };
 
--- js/movies.js
+++ js/movies.js
@@ -2,22 +2,26 @@
 const MOVIES = [
     {
         id: Math.random().toString(36).substring(2),
-        name: 'The kings men',
+        title: 'The kings men',
         rate: '42%',
+        yaer: 2021
     },
     {
         id: Math.random().toString(36).substring(2),
-        name: 'Sing 2',
+        title: 'Sing 2',
         rate: '69%',
+        year: 2015
     },
     {
         id: Math.random().toString(36).substring(2),
-        name: 'Spider-Man',
+        title: 'Spider-Man',
         rate: '93%',
+        year: 1999
     },
     {
         id: Math.random().toString(36).substring(2),
-        name: 'The Matrix',
+        title: 'The Matrix',
         rate: '63%',
+        year: 2005
     },
 ];
